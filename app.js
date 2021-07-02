bee6be21154a34079104ad0acc556eb2fdda8e16
andrewneves7 <42624686+andrewneves7@users.noreply.github.com>
2021-07-02 17:32:56 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+//commonJs
+ document.querySelectorAll('.watch-controls, controls a').forEach(control =>{
+     control.addEventListener('click', e => {
+         e.preventDefault();
+     })
+ })
+//End of Common JS
+
+
+
+//Cube
+let y = 20;
+let x = 0;
+let z = 0;
+let bool = true;
+let interval;
+
+
+const cube = document.querySelector('.cube')
+
+
+document.querySelector('.top-x-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${ x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.bottom-x-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${ x -= 20}deg) rotateY(${y}deg)
+    rotateZ(${z}deg)`
+})
+
+document.querySelector('.left-y-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${x}deg) rotateY(${y -= 20}deg) 
+    rotateZ(${z}deg)`
+})
+
+document.querySelector('.right-y-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${x}deg) rotateY(${y += 20}deg) 
+    rotateZ(${z}deg)`
+})
+
+document.querySelector('.top-z-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${x}deg) rotateY(${y}deg) 
+    rotateZ(${z -= 20}deg)`
+})
+document.querySelector('.bottom-z-control').addEventListener('click',()=>{
+    cube.style.transform =  `rotateX(${x}deg) rotateY(${y}deg) 
+    rotateZ(${z += 20}deg)`
+})
+
+
+const playPause = () =>{
+    if(bool){
+       interval = setInterval(()=>{
+            cube.style.transform =  `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`
+        },100) 
+    }else{
+        clearInterval(interval)
+    }
+    
+      
+}
+
+playPause()
+
+document.querySelector('.controls').addEventListener('mouseover',()=>{
+    bool = false
+    playPause()
+})
+
+
+document.querySelector('.controls').addEventListener('mouseout',()=>{
+    bool = true
+    playPause()
+})
+
+//End of Cube
+
+// slide show
+const slideshowDivs = () => {
+    for (let i = 1; i <= 5;i++){
+        const div = document.createElement('div')
+        
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`
+
+    
+        i === 1 && div.classList.add('change')
+
+
+    document.querySelector('.slideshow').appendChild(div)
+    }
+}
+
+slideshowDivs();
+
+
+const divs = document.querySelectorAll(".slideshow div");
+
+let a = 1
+
+
+const slideshow =() =>{
+    setInterval(()=>{
+        a++
+
+        const div = document.querySelector('.slideshow .change');
+        //console.log('this is current div')
+        //console.log(div)
+
+
+        div.classList.remove('change');
+        if (a < divs.length){
+            div.nextElementSibling.classList.add('change')
+        }else{
+            divs[0].classList.add('change')
+            a = 1
+        }
+       
+        // console.log('this is the next div')
+        // console.log(div.nextElementSibling)
+
+
+         //div.nextElementSibling.classList.add('change');
+
+
+    //console.log(nextNode)
+
+
+    },20000)
+} 
+
+slideshow();
+
+//section-3
+const section3Content = document.querySelector('.section-3-content')
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset + window.innerHeight >= section3Content.offsetTop + section3Content.offsetHeight / 2){
+    section3Content.classList.add('change')
+    }
+})
+
+//End of section-3
+
+// section-4
+const watchBands = document.querySelector('.watch-bands')
+
+const watchCases= document.querySelector('.watch-cases')
+
+
+const watchTopControl = document.querySelector('.watch-top-controls')
+const watchRightControl = document.querySelector('.watch-right-controls')
+const watchBottomControl = document.querySelector('.watch-bottom-controls')
+const watchLeftControl = document.querySelector('.watch-left-controls')
+
+let axisY = 0;
+let axisX = 0;
+
+watchTopControl.addEventListener('click',()=>{
+    watchCases.style.marginTop = `${axisY -= 70}rem`
+})
+watchBottomControl.addEventListener('click',()=>{
+    watchCases.style.marginBottom = `${axisY += 70}rem`
+})
+watchRightControl.addEventListener('click',()=>{
+    watchBands.style.marginRight = `${axisX += 70}rem`
+})
+watchLeftControl.addEventListener('click',()=>{
+    watchBands.style.marginLeft = `${axisX -= 70}rem`
+})
+//End of section-4
+
